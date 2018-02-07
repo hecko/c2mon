587f85e215ed717b8fafa97c58c86332ae8b433f
James Hamilton <james.hamilton@cern.ch>
2018-02-07 17:17:41 +0100

    #193: Fix MySQL support

--- c2mon-server/c2mon-server-test/src/main/java/cern/c2mon/server/test/DatabasePopulationRule.java
+++ c2mon-server/c2mon-server-test/src/main/java/cern/c2mon/server/test/DatabasePopulationRule.java
@@ -2,39 +2,33 @@ package cern.c2mon.server.test;
 
 import org.junit.rules.ExternalResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Use this class if you need a cleanly populated backup database at the
  * start of your tests.
- *
+ * <p>
  * Example:
  *
+ * @author Justin Lewis Salmon
  * @RunWith(SpringJUnit4ClassRunner.class)
  * @ContextConfiguration(classes = {
- *    CommonModule.class,
- *    CacheDbAccessModule.class,
- *    CacheLoadingModule.class,
- *    DatabasePopulationRule.class
+ * CommonModule.class,
+ * CacheDbAccessModule.class,
+ * CacheLoadingModule.class,
+ * DatabasePopulationRule.class
  * })
  * public class ControlTagLoaderDAOTest {
- *
- *    @Rule
- *    @Autowired
- *    public DatabasePopulationRule databasePopulationRule;
- *
- *    @Test
- *    ...
+ * @Rule
+ * @Autowired public DatabasePopulationRule databasePopulationRule;
+ * @Test ...
  * }
- *
- * @author Justin Lewis Salmon
  */
 @Configuration
 public class DatabasePopulationRule extends ExternalResource {
@@ -43,11 +37,14 @@ public class DatabasePopulationRule extends ExternalResource {
   private DataSource cacheDataSource;
 
   @Override
-  protected void before() {
-    ResourceDatabasePopulator populator = new ResourceDatabasePopulator(
-        new ClassPathResource("sql/cache-data-remove.sql"),
-        new ClassPathResource("sql/cache-data-insert.sql")
-    );
+  protected void before() throws SQLException {
+    ResourceDatabasePopulator populator = new ResourceDatabasePopulator(new ClassPathResource("sql/cache-data-remove.sql"));
+    if (cacheDataSource.getConnection().getMetaData().getURL().contains("mysql")) {
+      populator.addScript(new ClassPathResource("sql/cache-data-update-sequence.sql"));
+    } else {
+      populator.addScript(new ClassPathResource("sql/cache-data-alter-sequence.sql"));
+    }
+    populator.addScript(new ClassPathResource("sql/cache-data-insert.sql"));
     DatabasePopulatorUtils.execute(populator, cacheDataSource);
   }
 }
